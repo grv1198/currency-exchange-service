6e81a1b5d3d49e0b41f701851a95f51c63f8aed6
grv1198 <grv1198@gmail.com>
2019-01-29 21:19:47 +0530

    make exchangevaue as jpa entity

--- src/main/java/com/grv/microservices/currencyexchangeservice/bean/ExchangeValue.java
+++ src/main/java/com/grv/microservices/currencyexchangeservice/bean/ExchangeValue.java
@@ -1,12 +1,21 @@
 package com.grv.microservices.currencyexchangeservice.bean;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private Integer port;
